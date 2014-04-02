ffd0c385b575141cca2cb88a08545cc82a8e9d63
Enda McCormack <endamccormack0@gmail.com>
2014-04-02 19:20:43 +0100

    added a datamodel for the app and site

---
+++ Models/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HealthyCampusWebApp.Models
+{
+
+    public class RecipeContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double PrepTime { get; set; }
+        public double CookTime { get; set; }
+        public int DifficultyLevel { get; set; }
+
+        public virtual List<Tag> Tags { get; set; }
+        public virtual List<Ingredient> Ingredients { get; set; }
+    } 
+    
+}
---
+++ Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HealthyCampusWebApp.Models
+{
+    
+    public class Tag
+    {
+       public string TagName { get; set; } 
+    }
+}
