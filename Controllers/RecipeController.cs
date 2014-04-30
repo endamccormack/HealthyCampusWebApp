93f7371ca93f4c09272b16e4bca5cdf708ec9517
Enda McCormack <endamccormack0@gmail.com>
2014-04-30 13:23:13 +0100

    reenabled authentication

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -93,37 +93,65 @@ namespace HealthyCampusWebApp.Controllers
                     tags.Add(new Tag { TagName = (string)values[s] });
             }
 
-            foreach(string s in listKeys)
+            for (int i = 0; i < listKeys.Count(); i++)
             {
-                tags.Add(new HealthyCampusContext().Tags.Find(s));
+                using( var db = new HealthyCampusContext())
+                {
+                    tags.Add(db.Tags.Find((string)values[(string)listKeys.ElementAt(i)]));
+                }
             }
 
             //now get all ingredients
             IEnumerable<string> ingredientsKeys = keys.Where(m => m.Contains("ingredientName"));
-            IEnumerable<string> amountKeys = keys.Where(m => m.Contains("ingredientAmount"));
+            IEnumerable<string> amountKeys = keys.Where(m => m.Contains("ingredientAmounts"));
             IEnumerable<string> measurementKeys = keys.Where(m => m.Contains("ingredientAmountMeasure"));
             List<Ingredient> ingredients = new List<Ingredient>();
 
             for (int i = 0; i < ingredientsKeys.Count(); i++)
 			{
-                string name = (string)values[(string)ingredientsKeys.ElementAt(0)];
-                int grams = int.Parse((string)values[(string)amountKeys.ElementAt(0)]);
-                string type = (string)values[(string)measurementKeys.ElementAt(0)];
+                string name = (string)values[(string)ingredientsKeys.ElementAt(i)];
+                int grams = 1;
+                int.TryParse((string)values[(string)amountKeys.ElementAt(i)], out grams);
+                string type = (string)values[(string)measurementKeys.ElementAt(i)];
 
                 ingredients.Add(new Ingredient() {  Name = name,  AmountInGrams = grams, IngredientType = type });
 			}
-           
+
             try
             {
                 // TODO: Add insert logic here
                 using (var db = new HealthyCampusContext())
                 {
-                    recipe.Tags = tags;
+                    //recipe.Tags = tags;
+                    
                     recipe.Ingredients = ingredients;
                     recipe.ImageURL = fileNameForDb;
                     
                     db.Recipes.Add(recipe);
+
+                    List<string> tagNames = new List<string>();
+
+                    foreach (Tag t in tags)
+                    {
+                        tagNames.Add(t.TagName);
+                    }
+
+     
+                    List<string> tagsAlreadyInDb = (from theTags in db.Tags where tagNames.Contains(theTags.TagName) select theTags.TagName).ToList<string>();
+                    List<Tag> tagsNotInDB = tags.ToList<Tag>();
+
+                    tagsNotInDB.RemoveAll(t => tagsAlreadyInDb.Contains(t.TagName));
+
+                    foreach(Tag t in tagsNotInDB)
+                    {
+                        db.Tags.Add(t);
+                    }
                     db.SaveChanges();
+
+                    foreach(Tag t in tags)
+                    {
+                        db.Database.ExecuteSqlCommand("INSERT INTO [dbo].[TagRecipes] ([Tag_TagName], [Recipe_RecipeId]) VALUES ({0},{1});",t.TagName, recipe.RecipeId );
+                    }
                 }
                 return RedirectToAction("Index");
             }
@@ -131,7 +159,7 @@ namespace HealthyCampusWebApp.Controllers
             {
                 return View();
             }
-        }
+        }   
 
         //
         // GET: /Recipe/Edit/5
@@ -174,16 +202,34 @@ namespace HealthyCampusWebApp.Controllers
         [Authorize]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            try
-            {
-                // TODO: Add delete logic here
+            //try
+            //{
+                var db = new HealthyCampusContext();
+                var recipe = db.Recipes.Find(id);
+                db.Recipes.Remove(recipe);
+           
+
+                var allIngredients = db.Database.SqlQuery<Ingredient>("SELECT [IngredientId], [Name], [AmountInGrams], [IngredientType] FROM [Ingredients] WHERE [Recipe_RecipeId] =" + recipe.RecipeId).ToList();
+          
+
+                if (allIngredients != null)
+                {
+                    foreach (Ingredient i in allIngredients)
+                    {
+                        Ingredient theIng = (from ing in db.Ingredients where ing.IngredientId.Equals(i.IngredientId) select ing).FirstOrDefault();
+                       
+                        db.Ingredients.Remove(theIng);
+                    }
+                }
+                
+                db.SaveChanges();
 
                 return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
         }
         [HttpGet]
         public ActionResult Image(int id)
@@ -220,7 +266,11 @@ namespace HealthyCampusWebApp.Controllers
         {
             using (var db = new HealthyCampusContext())
             {
-                var allIngredients = from i in db.Ingredients select i;
+                //var allIngredients = from i in db.Ingredients
+                //                     join r in db.Recipes on i equals i 
+                //                     select i.;
+
+                var allIngredients = db.Database.SqlQuery <newIngredient>("SELECT [IngredientId], [Name], [AmountInGrams], [IngredientType], [Recipe_RecipeId] FROM [Ingredients]").ToList();
 
                 var jsonSerialiser = new JavaScriptSerializer();
                 string json = string.Format("{0}", JsonConvert.SerializeObject(allIngredients));
@@ -246,5 +296,14 @@ namespace HealthyCampusWebApp.Controllers
     class RecipeTag{
         public int RecipeId { get; set; }
         public string TagName { get; set; }
+    }
+
+    class newIngredient
+    {
+        public int IngredientId { get; set; }
+        public string Name { get; set; }
+        public int AmountInGrams { get; set; }
+        public string IngredientType { get; set; }
+        public int Recipe_RecipeId { get; set; }
     }
 }
